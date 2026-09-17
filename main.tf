30c732b7b5b91098974c61d2d4fae1defac94c44
febe89 <fullstack9020@gmail.com>
2026-09-18 01:00:22 +0530

    main changed commit

--- main.tf
+++ main.tf
@@ -24,12 +24,36 @@ resource "aws_security_group" "web_sg" {
   }
 }
 
+# resource "aws_instance" "web" {
+#   ami                    = data.aws_ami.ubuntu.id
+#   instance_type          = "t2.micro"
+#   subnet_id              = data.aws_subnets.default.ids[0]
+#   vpc_security_group_ids = [aws_security_group.web_sg.id]
+#   key_name               = "a2"
+
+#   user_data = <<-EOF
+#     #!/bin/bash
+#     apt-get update -y
+#     apt-get install -y nginx
+#     systemctl enable nginx
+#     systemctl start nginx
+#     echo "Hello from Terraform EC2" > /var/www/html/index.html
+#   EOF
+
+#   tags = {
+#     Name = "web-server"
+#     Environment = "test"
+#   }
+# }
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
   subnet_id              = data.aws_subnets.default.ids[0]
   vpc_security_group_ids = [aws_security_group.web_sg.id]
   key_name               = "a2"
+
+  # CRITICAL FIX: Forces AWS to give this instance a public IP address
+  associate_public_ip_address = true
 
   user_data = <<-EOF
     #!/bin/bash
@@ -41,7 +65,7 @@ resource "aws_instance" "web" {
   EOF
 
   tags = {
-    Name = "web-server"
+    Name        = "web-server"
     Environment = "test"
   }
 }
